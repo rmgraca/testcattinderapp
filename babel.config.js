ced12409e0959141b710f7de1348afc973f72795
Rui Graça <rui.m.a.graca@gmail.com>
2024-10-04 15:58:31 +0100

    configs

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,38 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module:react-native-dotenv',
+      {
+        envName: 'APP_ENV',
+        moduleName: '@env',
+        path: '.env',
+        blocklist: null,
+        allowlist: null,
+        safe: false,
+        allowUndefined: true,
+        verbose: false
+      }
+    ],
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', 'js', '.ts', '.tsx', '.json'],
+        alias: {
+          '~assets': './src/assets/',
+          '~app': './src/app/',
+          '~constants': './src/constants/',
+          '~hooks': './src/hooks/',
+          '~providers': './src/providers/',
+          '~components': './src/components/',
+          '~screens': './src/screens/',
+          '~types': './src/types/',
+          '~services': './src/services/',
+          '~root': './'
+        }
+      }
+    ],
+    ['@babel/plugin-transform-private-methods', { loose: true }]
+  ]
 };
